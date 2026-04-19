4636f260c3da773fe0ea7de0f9b3cbae1012c355
Facundo <diazortizfacundo@gmail.com>
2026-04-18 22:47:51 -0300

    punto 2: parte 1

---
+++ ArregloDeNombres.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//FUNCIONES
+void MostrarPersonas(char *V[]);
+
+//MAIN
+void main(){
+    char nombre[20];
+    char *V[5];
+    int i,longitud;
+
+    for(i=0;i<5;i++){
+        printf("Ingrese el nombre de la persona numero %d: ",i+1);
+        gets(nombre);
+        longitud = strlen(nombre);
+
+        V[i] = (char *)malloc(longitud * sizeof(char));
+        strcpy(V[i],nombre);
+        //puts(V[i]);
+    }
+    MostrarPersonas(V);
+    
+
+    //liberar memoria
+    for(i=0;i<5;i++){
+        free(V[i]);
+    }
+}
+
+//FUNCIONES
+void MostrarPersonas(char *V[]){
+    int i;
+    for(i=0;i<5;i++){
+        printf("Persona numero %d: ",i+1);
+        puts(V[i]);
+    }
+}
